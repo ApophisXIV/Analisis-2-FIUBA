ca10aff55e874e3dcd0a18a1792dbcbcd21f61dc
Guido E. Rodriguez <42053774+ApophisXIV@users.noreply.github.com>
2022-08-22 03:00:21 -0300

    🐛 FIX: Color Theme

--- src/storage.js
+++ src/storage.js
@@ -4,4 +4,4 @@ export const cards_state_db = writable(localStorage.getItem("an2_cards_state_db"
 cards_state_db.subscribe((cards) => localStorage.setItem("an2_cards_state_db", cards));
 
 export let theme = writable(localStorage.getItem("an2_theme") || "cupcake");
-theme.subscribe((v) => localStorage.setItem("theme", v));
+theme.subscribe((v) => localStorage.setItem("an2_theme", v));
